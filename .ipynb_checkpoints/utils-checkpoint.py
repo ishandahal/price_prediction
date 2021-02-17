0f28ff44ff2c0a254f9e2d2230973dda74be0329
ishan dahal <ishan.dahal88@gmail.com>
2021-02-16 19:45:35 -0500

    Add prediction and accuracy plots

---
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -0,0 +1,10 @@
+
+## Accuracy metrics (R2 score) from the models
+
+tree_validation_score = -1.3
+rf_validation_score = 0.22
+gb_validation_score = 0.27
+xgb_validation_score = -0.04
+bayes_validation_score = 0.19
+linear_regression = 0.15
+SVM = 0.13
